009ce4afa81cee106acd91c0a407cd68ccfd5567
Stefaniya Yanovich <stefanishy@gmail.com>
2021-03-08 14:43:37 +0300

    edit:styles main page 1920/1360

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import africaImg from "../assets/img/africa.png";
+import africaImg from "../assets/img/africa.jpg";
 
 const CountryCard = () => {
   return (
     <div className="country-card">
-      <img className={"card-img"} src={africaImg} alt="card"/>
+      <img className={"card-img"} src={africaImg} alt="card" />
       <span className="card-text">КЕНИЯ</span>
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <footer className={"footer"}>
       <div className="git-block">
-        <a href="https://github.com/oranje322">@oranje322</a>
-        <a href="https://github.com/kristinand">@kristinand</a>
-        <a href="https://github.com/ya-stefaniya">@ya-stefaniya</a>
-        <a href="https://github.com/Jears017">@Jears017</a>
+        <a href="https://github.com/oranje322"> &#9996; @oranje322</a>
+        <a href="https://github.com/kristinand">&#129310; @kristinand</a>
+        <a href="https://github.com/ya-stefaniya"> 	&#128588; @ya-stefaniya</a>
+        <a href="https://github.com/Jears017">&#129311; @Jears017</a>
       </div>
       <div className="course-block">
-        <img className={"footer-img"} src="https://rs.school/images/rs_school_js.svg" alt="rsschhol"/>
+        <img className={"footer-img"} src="https://rs.school/images/rs_school_js.svg" alt="rsschhol" />
         <span className="footer-text">/2021</span>
       </div>
     </footer>
