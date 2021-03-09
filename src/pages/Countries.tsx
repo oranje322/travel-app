307acf8adfd1696f6be71b1756f91641499192e2
Stefaniya Yanovich <stefanishy@gmail.com>
2021-03-09 23:17:27 +0300

    fix:styles

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../countries.scss";
 import Header from "../components/Header";
 import ImageGallery from 'react-image-gallery';
-import {useParams} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {IState} from "../redux/reducers/reducerTypes";
-import {Api} from "../api/api";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { IState } from "../redux/reducers/reducerTypes";
+import { Api } from "../api/api";
 
 interface paramTypes {
 	ISOCode: string
@@ -17,9 +17,9 @@ interface paramTypes {
 //todo виджеты
 
 const Countries = () => {
-	const {ISOCode} = useParams<paramTypes>()
+	const { ISOCode } = useParams<paramTypes>()
 
-	const lang = useSelector((state:IState) => state.lang)
+	const lang = useSelector((state: IState) => state.lang)
 	const country = useSelector((state: IState) => state.countries)
 		.filter(countryObj => countryObj.ISOCode === ISOCode)[0]
 
@@ -32,16 +32,26 @@ const Countries = () => {
 			setTemperature(Math.round(r.data.main.temp))
 			setTemperatureIcon(r.data.weather[0].icon)
 		})
-	},[])
+	}, [])
 
-
+	const myRenderItem = (props: any) => {
+		return <div className="image-gallery-container">
+			<img className="image-gallery-image" src={props.original} title="hello world" />
+			<span className="image-gallery-description">{props.description}
+			</span>
+			<p className="image-gallery-title">{props.originalTitle}</p>
+		</div>;
+	}
 	const images = [
 		...country.attractions.map(attr => {
-				return {
-					original: attr.imageURL,
-					thumbnail: attr.imageURL,
-				}
+			return {
+				original: attr.imageURL,
+				thumbnail: attr.imageURL,
+				description: attr.desc,
+				originalTitle: attr.name,
+				renderItem: myRenderItem
 			}
+		}
 		)
 	];
 
@@ -49,12 +59,12 @@ const Countries = () => {
 
 	return (
 		<div className={"countries"}>
-			<Header inputVisible={false}/>
+			<Header inputVisible={false} />
 
 			<div className="widgets-block">
 				{/* блок с виджетами */}
 				<div className="widgets-block_info">
-					<p className="weather"><img src={`http://openweathermap.org/img/wn/${temperatureIcon}.png`} alt=""/> {temperature} ℃</p>
+					<p className="weather"><img src={`http://openweathermap.org/img/wn/${temperatureIcon}.png`} alt="" /> {temperature} ℃</p>
 					<p className="currency">1£ = 1.3$</p>
 				</div>
 				<div className="widgets-block_time">
@@ -64,12 +74,12 @@ const Countries = () => {
 			</div>
 
 			<div className={"img-block"}>
-				<img className={"country-img"} src={country.imageURL} alt="country-img"/>
+				<img className={"country-img"} src={country.imageURL} alt="country-img" />
 			</div>
 			<div className="video-block">
 				<iframe title={country.country} width="1000" height="420" src={`https://www.youtube.com/embed/${country.videoURL}`} frameBorder="0"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-								allowFullScreen></iframe>
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen></iframe>
 			</div>
 			<div className="desc-block">
 				<p className="desc-text">
@@ -79,7 +89,7 @@ const Countries = () => {
 			<div className="gallery-block">
 				<h2 className={'subtitle'}>Что посмотреть?</h2>
 				<div className="slider">
-					<ImageGallery items={images}/>
+					<ImageGallery items={images} />
 				</div>
 			</div>
 			<div className="map">
