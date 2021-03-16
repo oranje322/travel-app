b208f9bd93fbebda5c81de6e56a9e994415bcc35
oranje <oranje322@mail.ru>
2021-03-16 18:51:11 +0300

    feat: add search button, fix styles select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,86 +1,93 @@
-import React, { useEffect } from "react";
-import { useTranslation } from 'react-i18next';
+import React, {useEffect} from "react";
+import {useTranslation} from 'react-i18next';
 import logo from "../assets/img/logo.png";
 import userImg from "../assets/img/log.png";
-import { Link } from "react-router-dom";
-import { IState } from "../redux/reducers/reducerTypes";
-import { changeSearchThunk, logoutThunk } from "../redux/thunk/thunk";
-import { useDispatch, useSelector } from "react-redux";
-import { Button } from "@material-ui/core";
-import { setUserData } from "../redux/actions/actions";
+import {Link} from "react-router-dom";
+import {IState} from "../redux/reducers/reducerTypes";
+import {changeSearchThunk, logoutThunk} from "../redux/thunk/thunk";
+import {useDispatch, useSelector} from "react-redux";
+import {Button} from "@material-ui/core";
+import {setUserData} from "../redux/actions/actions";
 import SelectLang from "./SelectLang/SelectLang";
 
 interface IHeader {
-  inputVisible: boolean;
+	inputVisible: boolean;
 }
 
-const Header = ({ inputVisible }: IHeader) => {
-  const userData = useSelector((state: IState) => state.userData);
-  const searchValue = useSelector((state: IState) => state.searchValue);
+const Header = ({inputVisible}: IHeader) => {
+	const userData = useSelector((state: IState) => state.userData);
+	const searchValue = useSelector((state: IState) => state.searchValue);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    const localUserData = localStorage.getItem("userData");
-    if (localUserData) {
-      dispatch(setUserData(JSON.parse(localUserData)));
-    }
-  }, []);
-  const { t, i18n } = useTranslation();
+	useEffect(() => {
+		const localUserData = localStorage.getItem("userData");
+		if (localUserData) {
+			dispatch(setUserData(JSON.parse(localUserData)));
+		}
+	}, []);
+	const {t, i18n} = useTranslation();
 
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(changeSearchThunk(e.target.value));
+	const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		dispatch(changeSearchThunk(e.target.value));
 
-  };
+	};
 
-  const handleKeyPressInput = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      dispatch(changeSearchThunk(searchValue));
-    }
-  };
+	const handleKeyPressInput = (e: React.KeyboardEvent) => {
+		if (e.key === "Enter") {
+			dispatch(changeSearchThunk(searchValue));
+		}
+	};
 
-  const logout = () => {
-    dispatch(logoutThunk());
-  };
+	const handleClickSearchBtn = (e: React.MouseEvent) => {
+		dispatch(changeSearchThunk(searchValue));
+	}
 
-  return (
-    <div className="container header">
-      <header className={"header"}>
-        <Link to={"/"}>
-          <div className="logo-block">
-            <img className={"logo"} src={logo} alt="logo" />
-          </div>
-        </Link>
-        <div className="side-block">
-          {inputVisible && (
-            <input
-              onChange={handleChangeInput}
-              onKeyPress={handleKeyPressInput}
-              value={searchValue}
-              autoFocus
-              className={"search-input"}
-              placeholder={t("search")}
-              type="search"
-            />
-          )}
-          <SelectLang/>
-          {userData.name && (
-            <img className={"user-img"} src={userData.photo || userImg} alt={userData.name} title={userData.name} />
-          )}
+	const logout = () => {
+		dispatch(logoutThunk());
+	};
 
-          {userData.name ? (
-            <Button size="large" onClick={logout}>
-              {t("exit")}
-            </Button>
-          ) : (
-              <Link style={{ textDecoration: "none" }} to="/login">
-                <Button size="large">{t("enter")}</Button>
-              </Link>
-            )}
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container header">
+			<header className={"header"}>
+				<Link to={"/"}>
+					<div className="logo-block">
+						<img className={"logo"} src={logo} alt="logo"/>
+					</div>
+				</Link>
+				<div className="side-block">
+					{inputVisible && (<>
+							<input
+								onChange={handleChangeInput}
+								onKeyPress={handleKeyPressInput}
+								value={searchValue}
+								autoFocus
+								className={"search-input"}
+								placeholder={t("search")}
+								type="search"
+							/>
+							<Button className={'search-button'} onClick={handleClickSearchBtn}
+											size={'large'}>🔎</Button>
+						</>
+					)}
+					<SelectLang/>
+					{userData.name && (
+						<img className={"user-img"} src={userData.photo || userImg} alt={userData.name} title={userData.name}/>
+					)}
+
+					{userData.name ? (
+						<Button size="large" onClick={logout}>
+							{t("exit")}
+						</Button>
+					) : (
+						<Link style={{textDecoration: "none"}} to="/login">
+							<Button size="large">{t("enter")}</Button>
+						</Link>
+					)}
+				</div>
+			</header>
+		</div>
+	);
 };
 
 export default Header;
